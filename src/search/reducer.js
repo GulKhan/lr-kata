7366056e829921fa01a8bdea5a29b41de1e3f861
GulKhan <gul@sourcenova.co.uk>
2018-10-14 19:22:43 +0100

    set initial state of the app

--- src/search/reducer.js
+++ src/search/reducer.js
@@ -1,6 +1,23 @@
 const { fromJS } = require('immutable')
 
 const initialState = fromJS({
+    results: [
+        {
+            "name": "hotelone",
+            "starRating": 5,
+            "facilities": ["car park", "pool"]
+        },
+        {
+            "name": "hoteltwo",
+            "starRating": 3,
+            "facilities": ["car park", "gym"]
+        },
+        {
+            "name": "hotelthree",
+            "starRating": 3,
+            "facilities": []
+        }
+    ]
 })
 
 export default (state = initialState, action) => {
